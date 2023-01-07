4ec621deadf16812ca74e41d2904143344ae5b96
Tidiane Diallo <diallotidiane014@gmail.com>
2023-01-07 14:44:53 +0000

    Building User class and using json-server for create backend server

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,24 +1,7 @@
 import { User } from "./models/User";
 
-const user = new User({name:"MaName",age: 25});
+const user = new User({});
+user.set({ name: 'kou bax ', age: 24 });
+user.save();
 
-user.on("change", () =>{
-    console.log("Change 1");
-});
-user.on("click", () =>{
-    console.log("click");
-});
-user.on("hover", () =>{
-    console.log("hover");
-});
-user.on("change", () =>{
-    console.log("Change 2");
-});
-user.on("wouloulou", () =>{
-    console.log("wouloulou");
-});
 
-user.trigger("change")
-user.trigger("wouloulou")
-user.trigger("click")
-user.trigger("hoverfeqs")
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from 'axios';
+
 interface UserProps {
+    id?: number;
     name?: string;
     age?: number;
 }
@@ -23,13 +26,32 @@ export class User {
         this.events[eventName] = handlers;
     }
 
-    trigger(eventName: string){
+    trigger(eventName: string) {
         const handlers = this.events[eventName];
-        if(!handlers || handlers.length === 0){
+        if (!handlers || handlers.length === 0) {
             return;
-        } 
+        }
         handlers.forEach(handler => handler());
     }
 
+    fetch(): void {
+        axios
+            .get(`http://localhost:3000/users/${this.get('id')}`)
+            .then((response: AxiosResponse): void => {
+                this.set(response.data);
+            }
+            );
+    }
+    
+    save(): void {
+        const id = this.get('id');
+        if(id){
+            axios.put(`http://localhost:3000/users/${id}`, this.data)
+        }
+        else{
+            axios.post(`http://localhost:3000/users/`, this.data)
+        }
+    }
 
-}
+
+}                   
