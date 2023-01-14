e649b176a4ee0e5a0ffa20baebb87214e74f5496
Tidiane Diallo <diallotidiane014@gmail.com>
2023-01-14 18:29:43 +0000

    Refactoring sync

--- web-Framework/src/models/User.ts
+++ web-Framework/src/models/User.ts
@@ -1,7 +1,7 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
-interface userProps {
+export interface UserProps {
     id?: number,
     name?: string,
     age?: number
@@ -9,32 +9,15 @@ interface userProps {
 
 
 export class User {
-   public events:Eventing  = new Eventing()
+    public events:Eventing  = new Eventing()
+    constructor(private data: UserProps) { };
 
-    constructor(private data: userProps) { };
-
-    get(propName: string): (string | number) {
+    get(propName: string): (string | number){
         return this.data[propName];
     }
 
-    set(update: userProps): void {
+    set(update: UserProps): void {
         Object.assign(this.data, update);
     }
 
-    
-    fetch(): void {
-        axios.get(`http://localhost:3000/users/${this.get('id')}`)
-            .then((response: AxiosResponse): void => {
-                this.set(response.data);
-            });
-    }
-
-    save(): void {
-        const id = this.get('id');
-        if (id) {
-            axios.put(`http://localhost:3000/users/${id}`, this.data);
-        } else {
-            axios.post('http://localhost:3000/users/', this.data);
-        }
-    }
 }
